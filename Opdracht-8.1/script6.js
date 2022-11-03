a0471f50aa49fb8fc245ba87e56a31f6d9ccab26
danielkuiper1 <9015587@student.tcrmbo.nl>
2022-11-03 22:54:23 +0100

    Opdracht-8.1

---
+++ Opdracht-8.1/script6.js
@@ -0,0 +1,26 @@
+function Plus() {
+  var g1 = parseInt(document.getElementById("x").value, 10);
+  var g2 = parseInt(document.getElementById("y").value, 10);
+  var calc = g1 + g2;
+  console.log(calc);
+}
+
+function Min() {
+  var g1 = parseInt(document.getElementById("x").value, 10);
+  var g2 = parseInt(document.getElementById("y").value, 10);
+  var calc = g1 - g2;
+  console.log(calc);
+}
+
+function Keer() {
+  var g1 = parseInt(document.getElementById("x").value, 10);
+  var g2 = parseInt(document.getElementById("y").value, 10);
+  var calc = g1 * g2;
+  console.log(calc);
+}
+function Deel() {
+  var g1 = parseInt(document.getElementById("x").value, 10);
+  var g2 = parseInt(document.getElementById("y").value, 10);
+  var calc = g1 / g2;
+  console.log(calc);
+}
