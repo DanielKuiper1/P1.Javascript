78f5a3c08564c8374214e01bf216efc77cf08478
danielkuiper1 <9015587@student.tcrmbo.nl>
2022-11-04 13:57:09 +0100

    c

--- Opdracht-8.1/script6.js
+++ Opdracht-8.1/script6.js
@@ -3,58 +3,58 @@ function Plus() {
   var g2 = parseInt(document.getElementById("y").value, 10);
   var calc = g1 + g2;
   console.log(calc);
-  document.getElementById("antwoord").innerHTML = (calc); 
-  if (g1 === 0) { 
+  document.getElementById("antwoord").innerHTML = calc;
+  if (g1 === 0) {
     let fout = "het getal is te laag";
     document.getElementById("antwoord").innerHTML = fout;
-    } 
-    if (g2 === 0) { 
-      let fout = "het getal is te laag";
-      document.getElementById("antwoord").innerHTML = fout;
-      } 
+  }
+  if (g2 === 0) {
+    let fout = "het getal is te laag";
+    document.getElementById("antwoord").innerHTML = fout;
+  }
 }
 function Min() {
   var g1 = parseInt(document.getElementById("x").value, 10);
   var g2 = parseInt(document.getElementById("y").value, 10);
   var calc = g1 - g2;
   console.log(calc);
-  document.getElementById("antwoord").innerHTML = (calc); 
-  if (g1 === 0) { 
+  document.getElementById("antwoord").innerHTML = calc;
+  if (g1 === 0) {
     let fout = "het getal is te laag";
     document.getElementById("antwoord").innerHTML = fout;
-    } 
-    if (g2 === 0) { 
-      let fout = "het getal is te laag";
-      document.getElementById("antwoord").innerHTML = fout;
-      } 
+  }
+  if (g2 === 0) {
+    let fout = "het getal is te laag";
+    document.getElementById("antwoord").innerHTML = fout;
+  }
 }
 function Keer() {
   var g1 = parseInt(document.getElementById("x").value, 10);
   var g2 = parseInt(document.getElementById("y").value, 10);
   var calc = g1 * g2;
   console.log(calc);
-  document.getElementById("antwoord").innerHTML = (calc); 
-  if (g1 === 0) { 
+  document.getElementById("antwoord").innerHTML = calc;
+  if (g1 === 0) {
     let fout = "het getal is te laag";
     document.getElementById("antwoord").innerHTML = fout;
-    } 
-    if (g2 === 0) { 
-      let fout = "het getal is te laag";
-      document.getElementById("antwoord").innerHTML = fout;
-      } 
+  }
+  if (g2 === 0) {
+    let fout = "het getal is te laag";
+    document.getElementById("antwoord").innerHTML = fout;
+  }
 }
 function Deel() {
   var g1 = parseInt(document.getElementById("x").value, 10);
   var g2 = parseInt(document.getElementById("y").value, 10);
   var calc = g1 / g2;
   console.log(calc);
-  document.getElementById("antwoord").innerHTML = (calc); 
-  if (g1 === 0) { 
-  let fout = "het getal is te laag";
-  document.getElementById("antwoord").innerHTML = fout;
-  } 
-  if (g2 === 0) { 
+  document.getElementById("antwoord").innerHTML = calc;
+  if (g1 === 0) {
     let fout = "het getal is te laag";
     document.getElementById("antwoord").innerHTML = fout;
-    } 
+  }
+  if (g2 === 0) {
+    let fout = "het getal is te laag";
+    document.getElementById("antwoord").innerHTML = fout;
+  }
 }
